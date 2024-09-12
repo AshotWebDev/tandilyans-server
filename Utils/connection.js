4246cba8518d17e5a40bf311a3852e14f7b04f62
AshotWebDev <poghosyan.01@list.ru>
2024-09-12 12:37:37 +0400

    mongo

--- Utils/connection.js
+++ Utils/connection.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
     //   useCreateIndex: true, // If using Mongoose 5.x, may be needed for unique indexes
     });
 
